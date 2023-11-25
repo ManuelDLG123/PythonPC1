30b7fe1353b7371f21ffe33c590d43dbdfddffea
ManuelDLG123 <152012023+ManuelDLG123@users.noreply.github.com>
2023-11-25 17:23:47 +0000

    repuesta 5ta pregunta

---
+++ 5ta Pregunta PC2.py
@@ -0,0 +1,20 @@
+""" Problema 5:
+Genere   una   función   que   tenga   como   parámetros   el   ingreso   de   un   número   entero   y   un   dígito. Verifique la cantidad de veces que se usa dicho número en el dígito solicitado.
+Ejemplo:
+Número ingresado: 15789000 y Dígito: 0
+Cantidad de veces 0 en el número: 4
+Nota:  Para  resolver  este  problema,  algunos  tipos  de datos dentro de python contemplan un método count. """
+
+
+def contar_digitos(numero, digito):
+    numero_str = str(numero)  
+    cantidad = numero_str.count(str(digito)) 
+    return cantidad
+
+# Ejemplo de uso
+numero_ingresado = 15789000
+digito = 0
+
+resultado = contar_digitos(numero_ingresado, digito)
+print(f"Número ingresado: {numero_ingresado} y Dígito: {digito}")
+print(f"Cantidad de veces {digito} en el número: {resultado}")

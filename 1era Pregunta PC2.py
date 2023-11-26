fd744e10a0c18f6e74aaef5a3abd8ef8cbde98a6
ManuelDLG123 <152012023+ManuelDLG123@users.noreply.github.com>
2023-11-26 18:08:29 -0500

    Update 1era Pregunta PC2.py

--- 1era Pregunta PC2.py
+++ 1era Pregunta PC2.py
@@ -2,14 +2,11 @@
 #Escribe  un  programa  en Python para encontrar los números que son divisibles 
 #por 7 y múltiplos de 5, en el rango de 1500 y 2700 (ambos incluidos).
 
-inicio_rango = 1500
-fin_rango = 2700
+rango=1500
 
-numeros_encontrados = []
+while rango <= 2700 :
+    if rango % 7==0 and rango % 5==0:
+        print(rango)
+    rango +=1
 
-for num in range(inicio_rango, fin_rango + 1):
-    if num % 7 == 0 and num % 5 == 0:
-        numeros_encontrados.append(num)
 
-print("Números divisibles por 7 y múltiplos de 5 en el rango de", inicio_rango, "a", fin_rango, "son:")
-print(numeros_encontrados)
